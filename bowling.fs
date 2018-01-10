362f3410ab66bc76d419247c5c2069c31a9f0b55
Adam Pridmore <adam_pridmore@hotmail.com>
2018-01-10 22:26:08 +0000

    Handles end of game spares and strikes.

--- bowling.fs
+++ bowling.fs
@@ -3,14 +3,19 @@ module Bowling
 open System
 open Xunit
 
-let rec score bowls = 
-    match bowls with
-    | [] -> 0
-    | a::b::c::rest when a = 10 -> a + b + c + score (b::c::rest) // Strike
-    | a::b::c::rest when a + b = 10 -> a + b + c + score (c::rest) // Spare
-    | a::b::rest -> a + b + (score rest)
-    | _ -> failwith (sprintf "Invalid bowls: %A" bowls)
-    
+let score bowls = 
+    let rec scoreFrames bowls frameIndex =
+        let nextFrameIndex = frameIndex + 1 
+        match frameIndex, bowls with
+        | _ , [] -> 0
+        | frameIndex, _ when frameIndex > 9 -> 0 // Ignore any bonus balls
+        | _ , a::b::c::rest when a = 10 -> a + b + c + scoreFrames (b::c::rest) nextFrameIndex // Strike
+        | _ , a::b::c::rest when a + b = 10 -> a + b + c + scoreFrames (c::rest) nextFrameIndex // Spare
+        | _ , a::b::rest -> a + b + (scoreFrames rest) nextFrameIndex
+        | _ -> failwith (sprintf "Invalid bowls: %A" bowls)
+
+    scoreFrames bowls 0
+       
 [<Fact>]
 let ``Simple bowl`` () =
     Assert.Equal(2, [1;1] |> score)
@@ -22,3 +27,18 @@ let ``Spare bowl`` () =
 [<Fact>]
 let ``Strike bowl`` () =
     Assert.Equal(16, [10;1;2] |> score)       
+
+[<Fact>]
+let ``End game spare`` () =
+    let bowls = List.concat [(Array.create 18 0) |> Array.toList ;[5;5;1] ]
+    Assert.Equal(11, bowls |> score)
+
+[<Fact>]
+let ``End game strike`` () =
+    let bowls = List.concat [(Array.create 18 0) |> Array.toList ;[10;1;2] ]
+    Assert.Equal(13, bowls |> score)
+
+[<Fact>]
+let ``Perfect Game`` () =
+    let bowls = (Array.create 12 10) |> Array.toList
+    Assert.Equal(300, bowls |> score)
