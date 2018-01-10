4ae7d88f534c4d1276857bcd296e2cc3f417a7c7
Adam Pridmore <adam_pridmore@hotmail.com>
2018-01-10 21:45:43 +0000

    - Slight simplification (and eliminated score for invalid games).

--- bowling.fs
+++ bowling.fs
@@ -9,16 +9,15 @@ let rec score bowls =
     | a::b::c::rest when a = 10 -> a + b + c + score (b::c::rest) // Strike
     | a::b::c::rest when a + b = 10 -> a + b + c + score (c::rest) // Spare
     | a::b::rest -> a + b + (score rest)
-    | [a] -> a
     | _ -> failwith (sprintf "Invalid bowls: %A" bowls)
     
 [<Fact>]
-let ``Single simple bowl`` () =
-    Assert.Equal(1, [1] |> score)
+let ``Simple bowl`` () =
+    Assert.Equal(2, [1;1] |> score)
 
 [<Fact>]
 let ``Spare bowl`` () =
-    Assert.Equal(12, [5;5;1] |> score)       
+    Assert.Equal(14, [5;5;1;2] |> score)
 
 [<Fact>]
 let ``Strike bowl`` () =
